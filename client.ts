7f7e59c6adac63eca0bfb6f2b1c98fd423c8a642
Alfons Andrade <alfons@alunos.utfpr.edu.br>
2023-09-24 17:21:58 -0300

    STORY: Adjusting chat interface and implemented file transfer server -> client

--- client.ts
+++ client.ts
@@ -1,39 +1,87 @@
-const readline = require('readline');
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-const socket = Bun.connect({
-  hostname: "localhost",
-  port: 8080,
-  socket: {
-    data(socket, message) {
-      const decodedMessage = message.toString(); // Decode buffer to string
-      console.log(`[Server Message]: ${decodedMessage}`);
-    },
-    open(socket) {
-      console.log("Connected to chat server!");
+let currentSocket;
 
-      rl.on('line', (line) => {
-        if (line === 'disconnect') {
-          console.log("Disconnecting from the server...");
-          socket.end();
-          rl.close();
-        } else {
-          socket.write(line);
-        }
-      });
-    },
-    close(socket) {
-      console.log("Disconnected from chat server.");
-      rl.close();
-    },
-    error(socket, error) {
-      console.error(`Client error: ${error}`);
-    },
-    end(socket) {
-      console.log("Connection closed by server.");
+function sendFileToServer(filePath: string) {
+    if (!currentSocket || typeof currentSocket.write !== 'function') {
+        console.error('Invalid socket object or socket not connected.');
+        return;
     }
-  },
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.on('data', (chunk) => {
+        currentSocket.write(`FILE_CHUNK:${chunk}`);
+    });
+
+    fileStream.on('end', () => {
+        currentSocket.write('FILE_END');
+    });
+}
+
+const BunSocket = Bun.connect({
+    hostname: "localhost",
+    port: 8080,
+    socket: {
+        data(socket, message) {
+            const decodedMessage = message.toString();
+            const date = new Date();
+            if (decodedMessage.startsWith('Nome:')) {
+                const lines = decodedMessage.split('\n');
+                const fileName = lines[0].split(': ')[1].trim();
+                const fileSize = parseInt(lines[1].split(': ')[1]);
+                const fileHash = lines[2].split(': ')[1].trim();
+                const fileData = lines[3].split(': ')[1];
+                const receivedHash = crypto.createHash('sha256').update(fileData).digest('hex');
+                if (receivedHash !== fileHash) {
+                    console.error('File hash mismatch!');
+                    return;
+                }
+                const newPath = `${date.toISOString()}_${fileName}`;
+                fs.writeFileSync(newPath, Buffer.from(fileData, 'hex'));
+                console.log(`Received and saved file: ${newPath}`);                
+                
+            } else if (decodedMessage === 'Disconnecting as per request.') {
+                console.log("Server requested to disconnect.");
+                socket.end();
+                rl.close();
+            } else {
+                console.log(`[Server]: ${decodedMessage}`);
+            }
+        },
+        open(socket) {
+            currentSocket = socket;
+            const sessionId = Date.now().toString(36);
+            console.log("Connected to chat server!");
+
+            rl.on('line', (line) => {
+                if (line.startsWith('sendfile:')) {
+                    const filePath = line.split(':')[1];
+                    sendFileToServer(filePath);
+                } else if (line.toLowerCase() === "exit") {
+                    console.log("Disconnecting from server...");
+                    socket.write("Sair");
+                } else {
+                    socket.write(line);
+                    console.log(`${sessionId}[Me]: ${line}`);
+                }
+            });
+        },
+        close(socket) {
+            console.log("Disconnected from chat server.");
+            rl.close();
+        },
+        error(socket, error) {
+            console.error(`Client error: ${error}`);
+        },
+        end(socket) {
+            console.log("Connection closed by server.");
+        }
+    },
 });
--- server.ts
+++ server.ts
@@ -1,59 +1,74 @@
-const readline = require('readline');
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-const clients = new Map(); // Map<sessionId, socket>
+const clients = new Map();
 
 Bun.listen({
-  hostname: "localhost",
-  port: 8080,
-  socket: {
-    data(socket, message) {
-      const decodedMessage = message.toString();
-      if (decodedMessage === 'CloseMASTER') {
-        console.log("Received CloseMASTER command from a client. Shutting down server...");
-        // Clean up and shut down
-        for (let clientSocket of clients.values()) {
-          clientSocket.write('Server is shutting down...');
-          clientSocket.end();
+    hostname: "localhost",
+    port: 8080,
+    socket: {
+        data(socket, message) {
+            const decodedMessage = message.toString();
+            const date = new Date();
+            console.log(`[Client-${socket.data.sessionId}][${date}]: ${decodedMessage}`);
+            if (decodedMessage === 'Sair') {
+                socket.write('Disconnecting as per request.');
+                socket.end();
+                clients.delete(socket.data.sessionId);
+                return;
+            }
+            const sessionId = Date.now().toString(36);
+            console.log(`${date}[${sessionId}]: ${decodedMessage}`);
+        },
+        open(socket) {
+            const sessionId = Date.now().toString(36);
+            socket.data = { sessionId };
+            clients.set(sessionId, socket);
+            socket.write(`You're connected with session ID: ${sessionId}`);
+        },
+        close(socket) {
+            clients.delete(socket.data.sessionId);
+        },
+        error(socket, error) {
+            console.error(`Error on socket: ${error}`);
         }
-        process.exit();
-      } else {
-        // Broadcast the message to all connected clients
-        for (let clientSocket of clients.values()) {
-          clientSocket.write(`${socket.data.sessionId}: ${decodedMessage}`);
-        }
-      }
     },
-    open(socket) {
-      const sessionId = Date.now().toString(36); // A simple unique ID
-      socket.data = { sessionId };
-      clients.set(sessionId, socket);
-      socket.write(`You're connected with session ID: ${sessionId}`);
-    },
-    close(socket) {
-      clients.delete(socket.data.sessionId);
-    },
-    error(socket, error) {
-      console.error(`Error on socket: ${error}`);
-    }
-  },
 });
 
-// Server-side terminal input
 rl.on('line', (line) => {
-  if (line === 'Close') {
-    console.log("Shutting down server...");
-    for (let clientSocket of clients.values()) {
-      clientSocket.write('Server is shutting down...');
-      clientSocket.end();
+    const data = new Date();
+    if (line.startsWith('Arquivo')) {
+        const fileName = line.split(' ')[1].trim();
+        if (fs.existsSync(fileName)) {
+            const fileData = fs.readFileSync(fileName);
+            const hash = crypto.createHash('sha256').update(fileData).digest('hex');
+            for (let clientSocket of clients.values()) {
+                clientSocket.write(`Nome: ${fileName}\nTamanho: ${fileData.length} bytes\nHash: ${hash}\nDados: ${fileData}\nStatus: ok`);
+            }
+            console.log(`[Server][${data}]: Sent file ${fileName} to clients.`);
+        } else {
+            for (let clientSocket of clients.values()) {
+                clientSocket.write(`Status: nok (Arquivo ${fileName} inexistente.)`);
+            }
+            console.log(`[Server][${data}]: File ${fileName} not found.`);
+        }
+    } else if (line === 'shut-down') {
+        console.log("Shutting down server...");
+        for (let clientSocket of clients.values()) {
+            clientSocket.write('Server is shutting down...');
+            clientSocket.end();
+        }
+        process.exit();
+    } else {
+        console.log(`[Server][${data}]: ${line}`);
+        for (let clientSocket of clients.values()) {
+            clientSocket.write(`[Server Chat]: ${line}`);
+        }
     }
-    process.exit();
-  } else {
-    for (let clientSocket of clients.values()) {
-      clientSocket.write(`[Server Broadcast]: ${line}`);
-    }
-  }
 });
